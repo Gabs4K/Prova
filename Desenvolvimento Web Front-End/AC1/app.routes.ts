5bc94b0b30b6e2717d72904d39955c1590862796
Gabs4K <gabrieldonisilva2006@hotmail.com>
2026-09-24 13:21:14 -0300

    Delete Desenvolvimento Web Front-End/AC1 directory

--- Desenvolvimento Web Front-End/AC1/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { CatalogoFilmes } from './catalogo-filmes/catalogo-filmes'; 
-// import { FormsModule } from '@angular/forms'
-
-export const routes: Routes = [
-//   {path: 'login',component: Login},
-    {path: 'CatalogoFilmes', component: CatalogoFilmes}
-];
